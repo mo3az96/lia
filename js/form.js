7bd8337fc4ae709ad340e9f396a2f0e2a5bcbc3c
mo3az96 <mo3az.abdellatif@gmail.com>
2025-10-04 23:28:58 +0300

    cart page

--- js/form.js
+++ js/form.js
@@ -25,6 +25,7 @@ $(document).ready(function () {
   if (typeof $.fn.select2 !== "undefined") {
     if ($(window).width() >= 992) {
       $("select[select2]").select2({
+        customClass: "error" ,
         minimumResultsForSearch: Infinity,
       });
     }
